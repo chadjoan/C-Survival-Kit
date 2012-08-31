43dc23db3cacbcf5b87ce40425cf1f7cd4c36f2f
Chad Joan <cjoan@gfschemicals.com>
2012-08-30 20:14:23 -0400

    This commit breaks most C code!  However, it is a vast improvement to the C exception handling implementation.  Things will get prettier and more robust when they are fixed.

--- EXAMPLES/TEST_UTIL.C
+++ EXAMPLES/TEST_UTIL.C
@@ -13,7 +13,7 @@ int baz()
 float qux()
 {
 	/*THROWV(NULL);*/
-	THROW_NUM(new_exception("qux does throw!\n"));
+	THROW(new_exception(10,"qux does throw!\n"));
 	printf("qux should not print this.\n");
 	return 0.0;
 }
@@ -26,24 +26,72 @@ void bar(int val1, int val2)
 int foo()
 {
 	/* TODO: bar(TRYR(baz()),TRYR(qux())); */
-	TRY_NUM(bar(baz(), qux()));
+	CALL(bar(baz(), qux()));
 	printf("This shouldn't get executed either.\n");
 	return 42;
 }
 
 void main()
 {
-	TRY_CATCH(baz())
+	TRY
+		baz();
 		printf("No exception thrown.\n");
-	CATCH(e)
+	CATCH(10, e)
 		print_exception(e);
 	ENDTRY
 	
 	printf("------\n");
 	
-	TRY_CATCH(foo())
+	TRY
+		TRY
+			printf("Nested TRY-TRY-CATCH test.\n");
+		CATCH(10, e)
+			print_exception(e);
+		ENDTRY
+		
+		foo();
 		printf("No exception thrown.\n");
-	CATCH(e)
+	CATCH(10, e)
+		TRY
+			printf("Nested CATCH-TRY-CATCH test.\n");
+		CATCH(10, e2)
+			printf("This shouldn't happen.\n");
+			/* print_exception(e2);*/
+		ENDTRY
+		
 		print_exception(e);
 	ENDTRY
+	
+	TRY
+		while(1)
+		{
+			TRY
+				printf("I'm in a loop.\n");
+				printf("Now to bail without stopping first!\n");
+				break;
+			CATCH(10,e)
+				printf("%s, %d: This shouldn't happen.", __FILE__, __LINE__);
+			ENDTRY
+		}
+	CATCH(BREAK_IN_TRY_CATCH, e)
+		print_exception(e);
+	ENDTRY
+	
+	
+	
+	TRY
+		while(1)
+		{
+			TRY
+				printf("I'm in a loop (again).\n");
+				printf("Now to bail without stopping first!\n");
+				continue;
+			CATCH(10,e)
+				printf("%s, %d: This shouldn't happen.", __FILE__, __LINE__);
+			ENDTRY
+		}
+	CATCH(CONTINUE_IN_TRY_CATCH, e)
+		print_exception(e);
+	ENDTRY
+	
 }
--- UTIL/ERR_UTIL.C
+++ UTIL/ERR_UTIL.C
@@ -9,7 +9,11 @@
 #include <lib$routines.h> /* lib$signal */
 
 frame_info  __frame_info_stack[FRAME_INFO_STACK_SIZE];
+jmp_buf     __frame_context_stack[FRAME_INFO_STACK_SIZE];
 ssize_t     __frame_info_end = 0;
+
+jmp_buf     __try_context_stack[TRY_CONTEXT_STACK_SIZE];
+ssize_t     __try_context_end = 0;
 
 	
 static void best_effort_vms_path_parse_device(
@@ -164,7 +168,7 @@ exception *no_exception()
 }
 #endif
 
-exception *new_exception(char *mess, ...)
+exception *new_exception(size_t error_code, char *mess, ...)
 {
 	va_list vl;
 	va_start(vl, mess);
@@ -172,7 +176,7 @@ exception *new_exception(char *mess, ...)
 	va_end(vl);
 	
 	exception *result = malloc(sizeof(exception));
-	/* result->error_code = error_code; */
+	result->error_code = error_code;
 	result->error_text = error_text_buffer;
 	return result;
 }
@@ -185,27 +189,60 @@ void die(char *mess, ...)
 	va_end(vl);
 	
 	perror(error_text_buffer);
-	/*exit(1);*/
-	lib$signal(EXIT_FAILURE);
+	exit(1);
+	/*lib$signal(EXIT_FAILURE);*/ /* This produced too much spam when exiting after a bunch of setjmp/longjmps. */
 }
 
-int __push_stack_info(size_t line, const char *file, const char *func)
+jmp_buf *__push_stack_info(size_t line, const char *file, const char *func)
 {
+	ERR_UTIL_TRACE("%s, %d: __push_stack_info(%d, %s, %s)\n", file, line, line, file, func);
 	frame_info fi;
 	fi.line_number = line;
 	fi.file_name = file;
 	fi.func_name = func;
-	__frame_info_stack[__frame_info_end++] = fi;
-	return 0;
+	fi.jmp_context = &__frame_context_stack[__frame_info_end];
+	__frame_info_stack[__frame_info_end] = fi;
+	__frame_info_end++;
+	return fi.jmp_context;
 }
 
 frame_info __pop_stack_info()
 {
-	frame_info fi = __frame_info_stack[__frame_info_end--];
+	__frame_info_end--;
+	frame_info fi;
+	
 	if ( __frame_info_end < 0 )
+	{
+		fi = __frame_info_stack[0];
 		die("Attempt to pop stack info at stack bottom.\r\n"		
-			"Happened in file %s at line %d in function %s\r\n",
+			"(probably) Happened in file %s at line %d in function %s\r\n",
 			fi.file_name, fi.line_number, fi.func_name);
+	}
+	
+	fi = __frame_info_stack[__frame_info_end];
+	ERR_UTIL_TRACE("__pop_stack_info(%d, %s, %s)\n", fi.line_number, fi.file_name, fi.func_name);
 
 	return fi;
+}
+
+
+jmp_buf *__push_try_context()
+{
+	if ( __try_context_end >= TRY_CONTEXT_STACK_SIZE )
+	{
+		die("Exceeded TRY stack size of %d.\n%s\n", TRY_CONTEXT_STACK_SIZE, stack_trace_to_str());
+	}
+	
+	return &__try_context_stack[__try_context_end++];
+}
+
+jmp_buf *__pop_try_context()
+{
+	__try_context_end--;
+	if ( __try_context_end < 0 )
+	{
+		die("TRY context stack pop with no matching push.\n%s\n", stack_trace_to_str());
+	}
+	
+	return &__try_context_stack[__try_context_end];
 }
