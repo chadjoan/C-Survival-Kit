4bce3a059ada237795e9d5700c2c6fdd3fc55d2d
chadjoan <cjoan@gfschemicals.com>
2012-09-20 23:20:43 -0400

    Add some exception defs for RMS stuff.  This belongs in another library, but that is too time-consuming to create right now.

--- survival_kit/feature_emulation/generated_exception_defs.h
+++ survival_kit/feature_emulation/generated_exception_defs.h
@@ -6,22 +6,27 @@
 
 #define skit_err_code ssize_t
 
-/* __EXC_TABLE_SIZE will be the number of definitions, plus 2 for the values 0 and 1 which aren't used for error codes. */
-
 int skit_exception_is_a( skit_err_code ecode1, skit_err_code ecode2 );
 
 /* Exception list notes: */
 /* Never use 0, it has a special meaning for setjmp/longjmp. */
 /* 1 is pretty dangerous too. */
+/* __SKIT_EXC_TABLE_SIZE will be the number of definitions, plus 2 for the values 0 and 1 which aren't used for error codes. */
+/*   (Easy calculation: take the last ID and add 1.) */
+/* The rest of the numbers must appear consecutively, or it will mess up the inheritance table. */
 /*	X(EXCEPTION_NAME,             PARENT_NAME,           ID,  "MESSAGE") */
-#define __SKIT_EXC_TABLE_SIZE 8
+#define __SKIT_EXC_TABLE_SIZE 11
 #define SKIT_EXCEPTION_LIST(X) \
-	X(SKIT_EXCEPTION,             SKIT_EXCEPTION,         2, "An exception was thrown.") \
-	X(SKIT_FATAL_EXCEPTION,       SKIT_FATAL_EXCEPTION,   3, "An unrecoverable exception was thrown.") \
-	X(SKIT_BREAK_IN_TRY_CATCH,    SKIT_EXCEPTION,         4, "Attempt to use the built-in 'break' statement while in an sTRY-sCATCH block.") \
-	X(SKIT_CONTINUE_IN_TRY_CATCH, SKIT_EXCEPTION,         5, "Attempt to use the built-in 'continue' statement while in an sTRY-sCATCH block.") \
-	X(SKIT_SOCKET_EXCEPTION,      SKIT_EXCEPTION,         6, "socket exception.") \
-	X(SKIT_OUT_OF_BOUNDS,         SKIT_EXCEPTION,         7, "Out of bounds exception.")
+	X(SKIT_EXCEPTION,             SKIT_EXCEPTION,          2, "An exception was thrown.") \
+	X(SKIT_FATAL_EXCEPTION,       SKIT_FATAL_EXCEPTION,    3, "An unrecoverable exception was thrown.") \
+	X(SKIT_BREAK_IN_TRY_CATCH,    SKIT_EXCEPTION,          4, "Attempt to use the built-in 'break' statement while in an sTRY-sCATCH block.") \
+	X(SKIT_CONTINUE_IN_TRY_CATCH, SKIT_EXCEPTION,          5, "Attempt to use the built-in 'continue' statement while in an sTRY-sCATCH block.") \
+	X(SKIT_SOCKET_EXCEPTION,      SKIT_EXCEPTION,          6, "socket exception.") \
+	X(SKIT_OUT_OF_BOUNDS,         SKIT_EXCEPTION,          7, "Out of bounds exception.") \
+	X(ASTD_IO_EXCEPTION,          SKIT_EXCEPTION,          8, "Generic I/O exception.") \
+	X(ASTD_FILE_IO_EXCEPTION,     ASTD_IO_EXCEPTION,       9, "File I/O exception.") \
+	X(ASTD_FILE_NOT_FOUND,        ASTD_FILE_IO_EXCEPTION, 10, "File not found.")
+/*               Don't forget to change __SKIT_EXC_TABLE_SIZE ^^           */
 
 #define SKIT_DEFINE_EXCEPTION_IDS(EXC_NAME, PARENT_NAME, UID, ERR_TXT) \
 	EXC_NAME = UID,
